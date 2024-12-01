97e541cf01c315b1f6ed50480e44cb419b177a34
mitulkheni <mitulkheni695@gmail.com>
2024-12-01 11:46:06 +0530

    added project images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Preloader from '../src/components/Pre';
 import Navbar from './components/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
-import Projects from './components/Projects/Projects';
+// import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
 import Resume from './components/Resume/ResumeNew';
 import {
@@ -57,6 +57,7 @@ function TrackedRoutes() {
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
+			{/* <Route path="/project" element={<Projects />} /> */}
 			<Route path="/resume" element={<Resume />} />
 			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,10 @@ import { Container, Row, Col } from 'react-bootstrap';
 import ProjectCard from './ProjectCards';
 import Particle from '../Particle';
 import leaf from '../../Assets/Projects/leaf.png';
-import emotion from '../../Assets/Projects/emotion.png';
-import editor from '../../Assets/Projects/codeEditor.png';
 import contractorForeman from '../../Assets/Projects/CF.png';
+import virtualBudz from '../../Assets/Projects/VirtualBudz.png';
+import Helloo from '../../Assets/Projects/Helloo.png';
 import suicide from '../../Assets/Projects/suicide.png';
-import bitsOfCode from '../../Assets/Projects/blog.png';
 
 function Projects() {
 	return (
@@ -32,18 +31,17 @@ function Projects() {
 
 					<Col md={4} className="project-card">
 						<ProjectCard
-							imgPath={bitsOfCode}
+							imgPath={virtualBudz}
 							isBlog={false}
 							title="Virtual Budz Campaign Management"
 							description="Virtual Budz is a campaign management app for cannabis, marijuana, and medical industry."
-							// ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
 							demoLink="https://virtualbudz.com/"
 						/>
 					</Col>
 
 					<Col md={4} className="project-card">
 						<ProjectCard
-							imgPath={editor}
+							imgPath={Helloo}
 							isBlog={false}
 							title="Helloo One"
 							description="Helloo one is an app to generate and share cards digitally between clients and customers"
@@ -70,19 +68,6 @@ function Projects() {
 							title="BIMX"
 							description="Developed BIMX, a construction management platform with 3D image handling and integrated AutoCAD viewer, helping contractors visualize and manage project designs efficiently."
 							ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-							// demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-						/>
-					</Col>
-
-					<Col md={4} className="project-card">
-						<ProjectCard
-							imgPath={emotion}
-							isBlog={false}
-							title="Face Recognition and Emotion Detection"
-							description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-							ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-							// demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
 						/>
 					</Col>
 				</Row>
